d9a720e1159d0682887d299296f8ede7e7c3cb23
ernieMcracken <ernie@lane-10.com>
2025-04-22 12:35:02 +0100

    configure docker to wait for db to be avaliable

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -20,7 +20,7 @@ class Command(BaseCommand):
                 self.check(databases=["default"])
                 db_up = True
             except (Psycopg2OpError, OperationalError):
-                self.stdout.wrtie("DB unavailable... waiting for 1 second")
+                self.stdout.write("DB unavailable... waiting for 1 second")
                 time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS("DB available"))
