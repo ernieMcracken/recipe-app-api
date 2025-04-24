244fbf932db1004970f03db203e18d6d8d57fc06
ernieMcracken <ernie@lane-10.com>
2025-04-24 16:52:25 +0100

    Tags functionality passing tests

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -17,23 +17,42 @@ class TagSerializer(serializers.ModelSerializer):
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipes"""
 
+    tags = TagSerializer(many=True, required=False)
+
     class Meta:
         model = Recipe
         fields = ["id", "title", "time_minutes", "price", "link", "tags"]
         read_only_fields = ["id"]
 
-    def create(self, validated_data):
-        """Create a recipe - nested serializer fields are read only, custom logic required"""
-        tags = validated_data.pop("tags", [])
-        recipe = Recipe.objects.create(**validated_data)
-        # context passed from view
+    def _get_or_create_tags(self, tags, recipe):
+        """Handle getting or creating tags"""
         auth_user = self.context["request"].user
 
         for tag in tags:
             tag_obj, created = Tag.objects.get_or_create(user=auth_user, **tag)
             recipe.tags.add(tag_obj)
 
+    def create(self, validated_data):
+        """Create a recipe - nested serializer fields are read only, custom logic required"""
+        tags = validated_data.pop("tags", [])
+        recipe = Recipe.objects.create(**validated_data)
+
+        self._get_or_create_tags(tags, recipe)
+
         return recipe
+
+    def update(self, instance, validated_data):
+        """Update a recipe"""
+        tags = validated_data.pop("tags", None)
+        if tags is not None:
+            instance.tags.clear()
+            self._get_or_create_tags(tags, instance)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
 
 
 class RecipeDetailSerializer(RecipeSerializer):
