556a536a1753f13d1b6064f19df6ffe8d8add0b9
ernieMcracken <ernie@lane-10.com>
2025-04-25 09:27:18 +0100

    adds ingredient viewset  and registers urls

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -4,7 +4,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe, Tag
+from core.models import Recipe, Tag, Ingredient
 from recipe import serializers
 
 
@@ -42,4 +42,17 @@ class TagViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         """Filter tags to the authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by("-name")
+
+
+class IngredientViewSet(viewsets.ModelViewSet):
+    """Manage Ingredients in the database"""
+
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter Ingredients to the authenticated user"""
         return self.queryset.filter(user=self.request.user).order_by("-name")
